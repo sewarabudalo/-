076943cd2031a5da3fa39c3a1e652bdc2ffbba70
sewarabudalo <113481049+sewarabudalo@users.noreply.github.com>
2022-09-14 02:33:22 +0300

    Add files via upload

---
+++ Malaffi.Storage/AttachmentManager.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Malaffi.Storage
+{
+    public static class AttachmentManager
+    {
+        public static void InsertAttachment(string fileName, Stream inputStream, string contentType, AttachmentTypes attachmentType, string fileUrl, string referanceId, int byteCount)
+        {
+            var generatedFileName = FileUploader.UploadFile(fileName, inputStream, contentType, attachmentType);
+            fileUrl = String.Format(fileUrl, generatedFileName);
+            Attachment attachment = new Attachment()
+            {
+                ID = Guid.NewGuid(),
+                AttachmentType = attachmentType,
+                CreatedOn = DateTime.UtcNow,
+                MeaningfulFileName = fileName,
+                MimeType = contentType,
+                Name = generatedFileName,
+                ReferenceID = referanceId,
+                CreatedBy = null,
+                URL = fileUrl,
+                Size = byteCount / 1024
+            };
+            AttachmentRepository.InsertAttachment(attachment);
+        }
+
+        public static void DeleteAttachment(Guid attachemntId)
+        {
+            AttachmentRepository.DeleteAttachment(attachemntId);
+        }
+
+        public static Attachment GetAttachment(Guid attachmentId)
+        {
+            return AttachmentRepository.GetAttachment(attachmentId);
+        }
+
+        public static string GetAttachmentURLByReferanceId(AttachmentTypes type, string referenceId)
+        {
+            var result = "../Content/Imgs/image-not-available.png";
+            var attachment = AttachmentRepository.GetAttachmentByReferanceId(type, referenceId);
+            if (attachment != null)
+            {
+                result = attachment.URL;
+            }
+            return result;
+        }
+
+        public static Attachment GetAttachmentByReferanceId(AttachmentTypes type, string referenceId)
+        {
+            Attachment result = AttachmentRepository.GetAttachmentByReferanceId(type, referenceId);
+            if (result != null)
+            {
+                return result;
+            }
+            return null;
+        }
+
+        public static IList<Attachment> GetAttachmentList(AttachmentTypes type, string referenceId)
+        {
+            return AttachmentRepository.GetAttachmentList(type, referenceId);
+        }
+
+        public static IList<string> GetAttachmentUrlList(AttachmentTypes type, string referenceId)
+        {
+            return AttachmentRepository.GetAttachmentList(type, referenceId).Select(a => a.URL).ToList();
+        }
+
+        public static List<string> GetAttachmentListURLByReferanceId(AttachmentTypes type, string referenceId)
+        {
+            var list = AttachmentRepository.GetAttachmentList(type, referenceId);
+            List<string> res = new List<string>();
+            if (list != null)
+            {
+                foreach (var item in list)
+                {
+                    res.Add(item.URL);
+                }
+
+                return res;
+            }
+
+            return null;
+        }
+
+        public static void UpdateAttachmentReferenceId(string oldRefernceId, string newReferenceId)
+        {
+            AttachmentRepository.UpdateAttachmentReferenceId(oldRefernceId, newReferenceId);
+        }
+    }
+}
---
+++ Malaffi.Storage/FileUploader.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+namespace Malaffi.Storage
+{
+    public class FileUploader
+    {
+        public static string UploadFile(string fileName, System.IO.Stream stream, string contentType, AttachmentTypes type)
+        {
+            try
+            {
+                var fileId = Guid.NewGuid();
+                string path = AppDomain.CurrentDomain.BaseDirectory + "Documents\\" + type + "\\";
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+                string filename = fileId + Path.GetExtension(fileName); ;
+
+                using (var fileStream = File.Create(Path.Combine(path, filename)))
+                {
+                    stream.Seek(0, SeekOrigin.Begin);
+                    stream.CopyTo(fileStream);
+                }
+                return filename;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static bool DeletePicture(string fileName, AttachmentTypes type)
+        {
+            try
+            {
+                string path = AppDomain.CurrentDomain.BaseDirectory + "Documents\\" + type + "\\";
+                string FilePath = Path.Combine(path, fileName);
+                System.IO.File.Delete(FilePath);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
