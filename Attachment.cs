10d99fe050f60f3476272bd08f2b907442b3d26a
sewarabudalo <113481049+sewarabudalo@users.noreply.github.com>
2022-09-14 02:39:25 +0300

    Add files via upload

---
+++ Attachment.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Malaffi.Storage
+{
+    public class Attachment
+    {
+        public Guid ID { get; set; }
+        public string MeaningfulFileName { get; set; }
+        public string ReferenceID { get; set; }
+        public AttachmentTypes AttachmentType { get; set; }
+        public string URL { get; set; }
+        public bool IsDeleted { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime ModifiedOn { get; set; }
+        public string Name { get; set; }
+        public string MimeType { get; set; }
+        public int Size { get; set; }
+    }
+
+    public enum AttachmentTypes
+    {
+        all = -1,
+        TestFile = 1,
+    }
+
+    public class Constants
+    {
+        public static string SP_InsertAttachment = "[dbo].[AttachmentInsert]";
+        public static string SP_GetAttachmentByID = "[dbo].[AttachmentGetByID]";
+        public static string SP_ListAttachments = "[dbo].[AttachmentsList]";
+        public static string SP_DeleteAttachment = "[dbo].[AttachmentDelete]";
+        public static string SP_UpdateAttachmentReferenceId = "[dbo].[AttachmentUpdateReferenceId]";
+        public static string SP_GetAttachmentByReferenceId = "[dbo].[AttachmentGetByReferenceId]";
+    }
+}
---
+++ AttachmentRepository.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading;
+
+namespace Malaffi.Storage
+{
+    public static class AttachmentRepository
+    {
+        public static readonly string scanAndMoreConnectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+        public static void InsertAttachment(Attachment attachment)
+        {
+            using (SqlConnection connection = new SqlConnection(scanAndMoreConnectionString))
+            {
+                SqlCommand command = new SqlCommand(Constants.SP_InsertAttachment, connection)
+                {
+                    CommandType = CommandType.StoredProcedure,
+                };
+                command.Parameters.AddWithValue("@ID", attachment.ID);
+                command.Parameters.AddWithValue("@ReferenceID", attachment.ReferenceID);
+                command.Parameters.AddWithValue("@AttachmentTypeID", (int)attachment.AttachmentType);
+                command.Parameters.AddWithValue("@URL", attachment.URL);
+                command.Parameters.AddWithValue("@Name", attachment.Name);
+                command.Parameters.AddWithValue("@CreatedBy", attachment.CreatedBy);
+                command.Parameters.AddWithValue("@MimeType", attachment.MimeType);
+                command.Parameters.AddWithValue("@Size", attachment.Size);
+
+                try
+                {
+                    connection.Open();
+                    int result = command.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("SQL EXCEPTION OCCURRED WHILE TRYING INSERT ATTACHMENT", ex);
+                }
+            }
+        }
+
+        public static Attachment GetAttachmentByReferanceId(AttachmentTypes type, string referenceId)
+        {
+            Attachment result = null;
+            using (SqlConnection connection = new SqlConnection(scanAndMoreConnectionString))
+            {
+                SqlCommand command = new SqlCommand(Constants.SP_GetAttachmentByReferenceId, connection)
+                {
+                    CommandType = System.Data.CommandType.StoredProcedure
+                };
+
+                command.Parameters.AddWithValue("@AttachmentTypeID", (int)type);
+                command.Parameters.AddWithValue("@ReferenceId", referenceId);
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        result = ConvertToAttachment(reader);
+                    }
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("SQL EXCEPTION OCCURRED WHILE TRYING TO GET ATTACHMENT BY REFERANCE ID", ex);
+                }
+            }
+            return result;
+        }
+
+        public static void DeleteAttachment(Guid attachemntId)
+        {
+            using (SqlConnection connection = new SqlConnection(scanAndMoreConnectionString))
+            {
+                SqlCommand command = new SqlCommand(Constants.SP_DeleteAttachment, connection)
+                {
+                    CommandType = CommandType.StoredProcedure,
+                };
+                command.Parameters.AddWithValue("@ID", attachemntId);
+                command.Parameters.AddWithValue("@ModifiedBy", Thread.CurrentPrincipal.Identity.Name);
+
+                try
+                {
+                    connection.Open();
+                    int result = command.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("SQL EXCEPTION OCCURRED WHILE TRYING TO DELETE ATTACHMENT", ex);
+                }
+            }
+        }
+
+        public static Attachment GetAttachment(Guid attachmentId)
+        {
+            Attachment result = new Attachment();
+            using (SqlConnection connection = new SqlConnection(scanAndMoreConnectionString))
+            {
+                SqlCommand command = new SqlCommand(Constants.SP_GetAttachmentByID, connection)
+                {
+                    CommandType = System.Data.CommandType.StoredProcedure
+                };
+
+                command.Parameters.AddWithValue("@ID", attachmentId);
+
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        result = ConvertToAttachment(reader);
+                    }
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("SQL EXCEPTION OCCURRED WHILE TRYING TO GET ATTACHMENT", ex);
+                }
+            }
+            return result;
+        }
+
+        public static IList<Attachment> GetAttachmentList(AttachmentTypes type, string referenceId)
+        {
+            IList<Attachment> result = new List<Attachment>();
+            using (SqlConnection connection = new SqlConnection(scanAndMoreConnectionString))
+            {
+                SqlCommand command = new SqlCommand(Constants.SP_ListAttachments, connection)
+                {
+                    CommandType = System.Data.CommandType.StoredProcedure
+                };
+
+                command.Parameters.AddWithValue("@AttachmentTypeID", (int)type);
+                command.Parameters.AddWithValue("@ReferenceId", referenceId);
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        var attachment = ConvertToAttachment(reader);
+                        result.Add(attachment);
+                    }
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("SQL EXCEPTION OCCURRED WHILE TRYING TO GET ATTACHMENT LIST", ex);
+                }
+            }
+            return result;
+        }
+
+        private static Attachment ConvertToAttachment(IDataReader rdr)
+        {
+            Attachment attachment = new Attachment();
+            if (rdr["ID"] != DBNull.Value)
+                attachment.ID = Guid.Parse(rdr["ID"].ToString());
+            if (rdr["AttachmentTypeID"] != DBNull.Value)
+                attachment.AttachmentType = (AttachmentTypes)Enum.Parse(typeof(AttachmentTypes), rdr["AttachmentTypeID"].ToString());
+            if (rdr["CreatedBy"] != DBNull.Value)
+                attachment.CreatedBy = rdr["CreatedBy"].ToString();
+            if (rdr["CreatedOn"] != DBNull.Value)
+                attachment.CreatedOn = DateTime.Parse(rdr["CreatedOn"].ToString());
+            if (rdr["IsDeleted"] != DBNull.Value)
+                attachment.IsDeleted = bool.Parse(rdr["IsDeleted"].ToString());
+            if (rdr["URL"] != DBNull.Value)
+                attachment.URL = rdr["URL"].ToString();
+            if (rdr["MimeType"] != DBNull.Value)
+                attachment.MimeType = rdr["MimeType"].ToString();
+            if (rdr["ModifiedBy"] != DBNull.Value)
+                attachment.ModifiedBy = rdr["ModifiedBy"].ToString();
+            if (rdr["ModifiedOn"] != DBNull.Value)
+                attachment.ModifiedOn = DateTime.Parse(rdr["ModifiedOn"].ToString());
+            if (rdr["Name"] != DBNull.Value)
+                attachment.Name = rdr["Name"].ToString();
+            if (rdr["Size"] != DBNull.Value)
+                attachment.Size = int.Parse(rdr["Size"].ToString());
+            if (rdr["ReferenceID"] != DBNull.Value)
+                attachment.ReferenceID = rdr["ReferenceID"].ToString();
+            return attachment;
+        }
+
+        public static void UpdateAttachmentReferenceId(string oldRefernceId, string newReferenceId)
+        {
+            using (SqlConnection connection = new SqlConnection(scanAndMoreConnectionString))
+            {
+                SqlCommand command = new SqlCommand(Constants.SP_UpdateAttachmentReferenceId, connection)
+                {
+                    CommandType = CommandType.StoredProcedure,
+                };
+                command.Parameters.AddWithValue("@OldRefernceId", oldRefernceId);
+                command.Parameters.AddWithValue("@NewReferenceId", newReferenceId);
+
+                try
+                {
+                    connection.Open();
+                    int result = command.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("SQL EXCEPTION OCCURRED WHILE TRYING TO UPDATE ATTACHMENT REFERENCE ID", ex);
+                }
+            }
+        }
+    }
+}
